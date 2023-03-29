2b20c85662242fa66af2ffd1d6ac3f51e4208c5e
Alfex4936 <ikr@kakao.com>
2023-03-29 23:26:47 +0900

    Add String part

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,12 +5,11 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base"],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
+    "space-before-function-paren": ["error", "always"],
   },
 };
